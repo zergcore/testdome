53cf5fae82abbf343d82e50c9284a0b5af268266
Zaidibeth Ramos <38891128+zergcore@users.noreply.github.com>
2022-11-30 15:46:24 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// React is loaded and is available as React and ReactDOM
+// imports should NOT be used
+const TodoItem = (props) => <li onClick={props.onClick}>{props.item.text}</li>
+
+class TodoList extends React.Component {
+  render() {
+    const { items, onListClick } = this.props;
+    return (<ul onClick={onListClick}>
+      {items.map((item, index) => 
+                 <TodoItem item={item} key={index} onClick={this.handleItemClick.bind(this, item)}/>)}
+    </ul>);
+  }
+  
+  handleItemClick(item, event) {
+    if (!item.done) {
+      event.persist();
+      this.props.onItemClick(item, event);
+    }
+  }
 }
+
+const App = (props) => <TodoList
+  items={props.items}
+  onListClick={(event) => console.log("List clicked!")}
+  onItemClick={(item, event) => { console.log(item, event) }}
+/>;
 
 export default App;
